a91e09c607b7ceb4091247a927628825bfa3d7d2
pjkui <pjkui@qq.com>
2016-03-18 16:18:44 +0800

    remove sample and fixed a bug : when user in index, js will cause a error

--- qa-behaviac-layer.php
+++ qa-behaviac-layer.php
@@ -1,18 +1,22 @@
 <?php
 
-	class qa_html_theme_layer extends qa_html_theme_base {
-		function head_script(){
-			qa_html_theme_base::head_script();
-			$css_str=qa_opt('site_url').'qa-plugin/'.AMI_PJK_EDITOR_DIR_NAME.'/plugins/codesnippet/lib/highlight/styles/default.css' ;
-			$this->output('<link rel="stylesheet" href="'.$css_str.'">');
-			$this->output(
-				'<script type="text/javascript">',
-				"if (typeof qa_wysiwyg_editor_config == 'object')",
-				"\tqa_wysiwyg_editor_config.skin='moono';",
-				'</script>'
+class qa_html_theme_layer extends qa_html_theme_base
+{
+    function head_script()
+    {
+        qa_html_theme_base::head_script();
+        if ($this->template == 'question') {
+            $css_str = qa_opt('site_url') . 'qa-plugin/' . AMI_PJK_EDITOR_DIR_NAME . '/plugins/codesnippet/lib/highlight/styles/default.css';
+            $this->output('<link rel="stylesheet" href="' . $css_str . '">');
+            $this->output(
+                '<script type="text/javascript">',
+                "if (typeof qa_wysiwyg_editor_config == 'object')",
+                "\tqa_wysiwyg_editor_config.skin='moono';",
+                '</script>'
 
-			);
-			$this->output('<script>hljs.initHighlightingOnLoad();</script>');
-		}
+            );
+            $this->output('<script>hljs.initHighlightingOnLoad();</script>');
+        }
+    }
 
-	}
+}
